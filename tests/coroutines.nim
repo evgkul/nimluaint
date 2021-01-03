f4c9b7335def212e5eb44bdd6e6eb301ebd09c9b
evgkul <evgenkulch@gmail.com>
2021-01-03 15:08:02 +0500

    ...

--- tests/coroutines.nim
+++ tests/coroutines.nim
@@ -14,16 +14,20 @@ test "lua_coroutines":
   g.registerMethods:
     proc testfn(args:LuaMultivalue[int]) =
       check args.seq == @[1,2,3]
-  g.rawget("testfn",LuaReference).call((1,2,3),void)
+    proc testfn2(a,b,c:int) =
+      let args = (a,b,c)
+      check args == (1,2,3)
+  #g.rawget("testfn",LuaReference).call((1,2,3),void)
   lua.load("""
 print("Test1")
-testfn(1,2,3)
+--testfn(1,2,3)
 local fn = function()
   testfn(1,2,3)
+  testfn2(1,2,3)
 end
-print("Test2")
-fn()
-print("Test3")
+--print("Test2")
+--fn()
+--print("Test3")
 local coro = coroutine.create(fn)
 coroutine.resume(coro)
   """).call((),void)
